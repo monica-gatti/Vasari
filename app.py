2d326f78ec59239935008141e9c942f7865f0656
Monica Gatti <gatti_monica@libero.it>
2020-09-30 01:27:08 +0200

    set up connection to neo4j in app.py

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ from controller.CulturalObjectController import culturalobject_bp
 from controller.VisitorController import visitor_bp
 from controller.VisitController import visit_bp
 import logging
+import os
+from neomodel import config
 
 app = Flask(__name__)
 api = Api(app, version="1.0", title="Vasari API", description="List, get, create, update a CulturalSite",)
@@ -18,9 +20,16 @@ app.register_blueprint(culturalobject_bp)
 app.register_blueprint(visitor_bp)
 app.register_blueprint(visit_bp)
 
-
-
 if __name__ == '__main__':
+    app_environment = os.environ['VASARI_ENV']
+    config.NEO4J_USERNAME = os.environ['NEO4J_USERNAME']
+    config.NEO4J_PASSWORD = os.environ['NEO4J_USERNAME']
+    if (app_environment == 'local'):
+        config.DATABASE_URL = 'bolt://neo4j:gfd@localhost:7687' 
+    elif (app_environment == 'container'):
+        config.DATABASE_URL = 'bolt://neo4j:test@database:7687' # os.environ['NEO4J_BOLT_URL']
+    config.ENCRYPTED_CONNECTION = False
+    config.MAX_POOL_SIZE = 50
+    #app.run(debug=True)
     app.debug = True
     app.run(host = '0.0.0.0',port=5000)
-    #app.run(debug=True)
--- settings/configuration.py
+++ settings/configuration.py
@@ -1,9 +1,20 @@
 # !/usr/bin/python
 # coding=utf-8 
 from neomodel import config
-config.NEO4J_USERNAME = 'neo4j'
-config.NEO4J_PASSWORD = 'neo4j'
-#'V@sar!2020'
-config.ENCRYPTED_CONNECTION = False
-config.MAX_POOL_SIZE = 50
-config.DATABASE_URL = 'bolt://neo4j:V@sar!2020@localhost:7687'
+import os
+
+app_environment = os.environ['VASARI_ENV']
+print(app_environment)
+if (app_environment == 'local'):
+    config.NEO4J_USERNAME = 'neo4j'
+    config.NEO4J_PASSWORD = 'neo4j'
+    #'V@sar!2020'
+    config.ENCRYPTED_CONNECTION = False
+    config.MAX_POOL_SIZE = 50
+    config.DATABASE_URL = 'bolt://neo4j:V@sar!2020@0.0.0.0:7687'
+elif (app_environment == 'container'):
+    config.NEO4J_USERNAME = 'neo4j'
+    config.NEO4J_PASSWORD = 'test'
+    config.ENCRYPTED_CONNECTION = False
+    config.MAX_POOL_SIZE = 50
+    config.DATABASE_URL = 'bolt://neo4j:V@sar!2020@:data7687'
